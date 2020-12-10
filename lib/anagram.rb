ae4659f75e7dd78c46642b6e7a3b8466125149d3
chrislemus <icrislemus@gmail.com>
2020-12-10 15:45:57 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :match 
+  def initialize(listen)
+    @match = listen
+  end
+  def match(words)
+    words.select {|word| word.split("").sort == @match.split("").sort}
+  end
+end
+
+# diaper = Anagram.new("diaper")
+# diaper.match(%w(hello pants dipper))
